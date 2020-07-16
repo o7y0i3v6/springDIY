089e1730e15138c473af8a2a8816606c9e9af3ae
o7y0i3v6 <mcunbkte720096@163.com>
2020-07-16 23:15:10 +0800

    update

---
+++ src/main/java/cn/blue/mvcFramework/annotation/BlueController.java
@@ -0,0 +1,19 @@
+package cn.blue.mvcFramework.annotation;
+
+import javax.xml.bind.Element;
+import java.lang.annotation.*;
+
+/**
+ * @author blue
+ * @ClassName BlueController
+ * @Description @Target({ElementType.TYPE})表示
+ * 这个注解只能作用在class上，@Retention 表示是运行时阶段。
+ *
+ * @date 2020/7/16 20:18
+ */
+@Target({ElementType.TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BlueController {
+    String value() default "";
+}
